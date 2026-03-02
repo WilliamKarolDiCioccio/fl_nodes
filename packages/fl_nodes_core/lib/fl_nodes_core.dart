8ee228be84b348b895a20f32bf11d53bc25904e6
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-03-02 08:41:19 +0100

    chore: export missing public types from fl_nodes_core barrel file

--- packages/fl_nodes_core/lib/fl_nodes_core.dart
+++ packages/fl_nodes_core/lib/fl_nodes_core.dart
@@ -1,5 +1,10 @@
-export 'package:fl_nodes_core/src/core/controller/callback.dart' show FlCallbackType;
+export 'package:fl_nodes_core/src/core/controller/callback.dart' show FlCallback, FlCallbackType;
 export 'package:fl_nodes_core/src/core/controller/core.dart' show FlNodesConfig, FlNodesController;
+export 'package:fl_nodes_core/src/core/controller/project.dart'
+    show ProjectCreator, ProjectLoader, ProjectSaver;
+export 'package:fl_nodes_core/src/core/controller/runner.dart'
+    show ExecutionHelperState, FlNodeExecutionState;
+export 'package:fl_nodes_core/src/core/events/bus.dart' show NodeEditorEventBus;
 export 'package:fl_nodes_core/src/core/events/events.dart'
     show
         FlAddLinkEvent,
@@ -14,23 +19,40 @@ export 'package:fl_nodes_core/src/core/events/events.dart'
         FlDragSelectionStartEvent,
         FlDrawTempLinkEvent,
         FlFieldEventType,
+        FlGraphBuildAbortedEvent,
+        FlGraphBuildCompleteEvent,
+        FlGraphBuildStartEvent,
+        FlGraphRunAbortedEvent,
+        FlGraphRunCompleteEvent,
+        FlGraphRunStartEvent,
+        FlHoverEventType,
+        FlLinkLabelEvent,
         FlLinkSelectionEvent,
         FlLoadProjectEvent,
         FlLocaleChangeEvent,
         FlNewProjectEvent,
+        FlNodeCustomDataEvent,
+        FlNodeCustomDataLayoutEvent,
+        FlNodeCustomDataPaintEvent,
+        FlNodeExecutionStateEvent,
         FlNodeFieldEvent,
+        FlNodeHoverEvent,
         FlNodeSelectionEvent,
         FlOverlayChangedEvent,
         FlPasteSelectionEvent,
         FlRemoveLinkEvent,
         FlRemoveNodeEvent,
         FlSaveProjectEvent,
+        FlSelectionEventType,
         FlStyleChangeEvent,
         FlViewportOffsetEvent,
-        FlViewportZoomEvent;
+        FlViewportZoomEvent,
+        NodeEditorEvent;
 export 'package:fl_nodes_core/src/core/localization/delegate.dart';
 export 'package:fl_nodes_core/src/core/models/data.dart'
     show
+        DataHandler,
+        EditorBuilder,
         FlControlInputPortPrototype,
         FlControlOutputPortPrototype,
         FlDataInputPortPrototype,
@@ -44,12 +66,19 @@ export 'package:fl_nodes_core/src/core/models/data.dart'
         FlNodeDataModel,
         FlNodePrototype,
         FlNodeState,
+        FlNodesGroupDataModel,
+        FlNodesProjectDataModel,
         FlPortDataModel,
         FlPortGeometricOrientation,
         FlPortPrototype,
         FlPortState,
+        LocalizedString,
+        OnNodeExecute,
+        OnVisualizerTap,
         PortLocator;
 export 'package:fl_nodes_core/src/core/models/overlay.dart';
+export 'package:fl_nodes_core/src/core/utils/misc/nodes.dart' show FlNodesUtils;
+export 'package:fl_nodes_core/src/core/utils/rendering/renderbox.dart' show RenderBoxUtils;
 export 'package:fl_nodes_core/src/styles/styles.dart'
     show
         FlFieldStyle,
@@ -63,13 +92,26 @@ export 'package:fl_nodes_core/src/styles/styles.dart'
         FlNodesStyle,
         FlPortShape,
         FlPortStyle,
+        LinkStyleBuilder,
+        NodeHeaderStyleBuilder,
+        NodeStyleBuilder,
+        PortStyleBuilder,
         flDefaultLinkStyleBuilder,
         flDefaultNodeHeaderStyleBuilder,
         flDefaultNodeStyleBuilder,
         flDefaultPortStyleBuilder;
 export 'package:fl_nodes_core/src/widgets/base_node.dart';
+export 'package:fl_nodes_core/src/widgets/builders.dart'
+    show
+        NodeBuilder,
+        NodeFieldBuilder,
+        NodeHeaderBuilder,
+        NodePortBuilder,
+        ShowCanvasContextMenu,
+        ShowLinkContextMenu,
+        ShowNodeContextMenu,
+        ShowNodeCreationtMenu,
+        ShowPortContextMenu;
 export 'package:fl_nodes_core/src/widgets/default_node.dart';
 export 'package:fl_nodes_core/src/widgets/node_editor.dart';
 export 'package:fl_nodes_core/src/widgets/node_editor_shortcuts.dart';
-export 'package:fl_nodes_core/src/core/utils/misc/nodes.dart' show FlNodesUtils;
-export 'package:fl_nodes_core/src/core/utils/rendering/renderbox.dart' show RenderBoxUtils;
